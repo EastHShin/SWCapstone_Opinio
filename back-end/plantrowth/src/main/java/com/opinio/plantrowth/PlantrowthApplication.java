f822dbd1fd304830d4e0ea227995e878c3b5f49c
EastHShin <xldk78@gmail.com>
2021-10-15 16:45:39 +0900

    add backend test

---
+++ back-end/plantrowth/src/main/java/com/opinio/plantrowth/PlantrowthApplication.java
@@ -0,0 +1,13 @@
+package com.opinio.plantrowth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantrowthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlantrowthApplication.class, args);
+	}
+
+}
---
+++ back-end/plantrowth/src/main/java/com/opinio/plantrowth/api/TestController.java
@@ -0,0 +1,14 @@
+package com.opinio.plantrowth.api;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/api/test")
+    public String test() {
+        return "test api";
+    }
+}
