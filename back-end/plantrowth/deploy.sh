225fad196cd8299e7e3ae7f3d591e9b21122fcbb
Donghoon Shin <64204666+EastHShin@users.noreply.github.com>
2021-10-17 03:41:22 +0900

    Create deploy.sh

---
+++ back-end/plantrowth/deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+REPOSITORY=/home/ec2-user/plantrowth
+cd $REPOSITORY
+
+APP_NAME=plantrowth
+JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep '.jar' | tail -n 1)
+JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
+
+CURRENT_PID=$(pgrep -f $APP_NAME)
+
+if [ -z $CURRENT_PID]
+then
+        echo "> no stop."
+else
+        echo "> kill -9 $CURRENT_PID"
+        kill -9 $CURRENT_PID
+        sleep 5
+fi
+
+echo "> $JAR_PATH deploy"
+nohup java -jar -Dspring.profiles.active=prod $JAR_PATH > /dev/null 2> /dev/null &
