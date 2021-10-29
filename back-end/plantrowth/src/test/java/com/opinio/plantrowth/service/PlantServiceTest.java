666b78aad07065e77067d03151847903e3d9f38e
EastHShin <xldk78@gmail.com>
2021-10-29 17:45:42 +0900

    식물목록 조회 Service layer 테스트 코드 작성

--- back-end/plantrowth/src/test/java/com/opinio/plantrowth/service/PlantServiceTest.java
+++ back-end/plantrowth/src/test/java/com/opinio/plantrowth/service/PlantServiceTest.java
@@ -4,6 +4,7 @@ import com.opinio.plantrowth.api.dto.CreatePlantRequestDto;
 import com.opinio.plantrowth.domain.Plant;
 import com.opinio.plantrowth.domain.User;
 import com.opinio.plantrowth.repository.PlantRepository;
+import com.opinio.plantrowth.repository.UserRepository;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -14,15 +15,19 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class PlantServiceTest {
     @Mock
     private PlantRepository plantRepository;
+    @Mock
+    private UserRepository userRepository;
 
     @InjectMocks
     private PlantService plantService;
@@ -54,16 +59,35 @@ class PlantServiceTest {
         User user = new User();
         user.setEmail("xlddd@gmail.com");
         user.setPassword("fffff");
+        user.setId(1L);
+
+        Plant plant = getPlant(user, "장미", "토로리",
+                LocalDate.now(), 0, 3, 2);
+        Plant plant2 = getPlant(user, "가시", "토리이",
+                LocalDate.now(), 0, 3, 2);
+        List<Plant> plants = new ArrayList<>();
+        plants.add(plant);
+        plants.add(plant2);
+        given(plantRepository.findAllByUserId(1L)).willReturn(plants);
+        //when
+        List<Plant> findPlants = plantService.findPlants(user.getId());
+        //then
+        Assertions.assertThat(findPlants.size()).isEqualTo(2);
+        Assertions.assertThat(findPlants.get(0)).isSameAs(plant);
+        Assertions.assertThat(findPlants.get(1)).isSameAs(plant2);
+    }
+
+    private Plant getPlant(User user, String species, String name,
+                           LocalDate birth, int exp, int supply,
+                           int cycle) {
         Plant plant = new Plant();
         plant.setUser(user);
-        plant.setPlantSpecies("장미");
-        plant.setPlantName("토로리");
-        plant.setPlantBirth(LocalDate.now());
-        plant.setPlantExp(0);
-        plant.setWaterSupply(10);
-        plant.setAlarmCycle(2);
-
-        //when
-        //then
+        plant.setPlantSpecies(species);
+        plant.setPlantName(name);
+        plant.setPlantBirth(birth);
+        plant.setPlantExp(exp);
+        plant.setWaterSupply(supply);
+        plant.setAlarmCycle(cycle);
+        return plant;
     }
 }
