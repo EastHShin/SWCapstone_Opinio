98e764929645b800b0a6e130621598bb78636146
skysaver00 <skysaver00@naver.com>
2021-11-01 19:30:23 +0900

    모델 백엔드 + 모델 생성 Python 코드 추가

--- back-end-deeplearning/app.py
+++ back-end-deeplearning/app.py
@@ -1,12 +1,45 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename
+
+import os
+from PIL import Image
+
+import fastbook
+from fastbook import *
+
+import pathlib
+temp = pathlib.PosixPath
+pathlib.PosixPath = pathlib.WindowsPath
 
 app = Flask(__name__)
 
+#업로드 HTML 렌더링
+@app.route('/upload')
+def render_file():
+   return render_template('upload.html')
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+#파일 업로드 처리
+@app.route('/fileUpload', methods = ['GET', 'POST'])
+def upload_file():
+   if request.method == 'POST':
+      f = request.files['file']
+      #저장할 경로 + 파일명
+      f.save(secure_filename(f.filename))
 
+      learner = load_learner('./plantrowth_resnet50_train.pkl')
+      print('model loaded!')
+
+      os.remove('test.jpg')
+      img = Image.open(f.filename)
+      img_resize = img.resize((224, 224))
+      img_resize.save('test.jpg')
+
+      answer = learner.predict('./test.jpg')
+      print(answer)
+
+      
+      #return 'uploads 디렉토리 -> 파일 업로드 성공!'
 
 if __name__ == '__main__':
-    app.run()
+    #서버 실행
+   app.run(debug = True)
