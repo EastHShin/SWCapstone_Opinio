538e3f43c9f9b33d72d86d5575c835cd98319750
skysaver00 <skysaver00@naver.com>
2021-11-04 21:19:45 +0900

    백엔드 수정

--- back-end-deeplearning/app.py
+++ back-end-deeplearning/app.py
@@ -52,11 +52,7 @@ def upload_file():
 
       print(list_1[2][value] * 100)
 
-      #print(answer)
-      #print(type(answer))
-
       return render_template('answer.html', first=list_1[0], second=list_2[0], value2=list_1[2][value] * 100, value1=list_2[2][value2] * 100)
-      #return 'uploads 디렉토리 -> 파일 업로드 성공!'
 
 if __name__ == '__main__':
     #서버 실행
