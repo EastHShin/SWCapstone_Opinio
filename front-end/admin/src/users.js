f0a1617b4f79dcc41fdb63254ef6a730796e3634
skysaver00 <skysaver00@naver.com>
2021-11-13 20:15:50 +0900

    유저 정보 검색 버튼 추가

--- front-end/admin/src/users.js
+++ front-end/admin/src/users.js
@@ -1,13 +1,21 @@
 import React from "react";
 import NavBar from "./NavBar.js";
 import Table from './usersbackenddummy';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 import './users.css';
+import UserIcon from '@material-ui/icons/Person';
 
 function Users() {
     return (
         <div className="front">
             <NavBar class="top" />
-            <h1> this is user page</h1>
+            <h1>유저정보</h1>
+            <UserIcon fontSize="large"></UserIcon>
+            <Form.Group class="inline" controlId="form.Email" style={{width: "370px"}}>
+                <Form.Control type="email" placeholder="아이디 번호 탐색"/>
+            </Form.Group>
+            <Button variant="success">검색</Button>
             <Table/>
         </div>
     );
--- front-end/admin/src/usersbackenddummy.js
+++ front-end/admin/src/usersbackenddummy.js
@@ -1,6 +1,8 @@
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 import * as ReactBootStrap from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
+import './users.css';
 
 const Table = () => {
     const [posts, setPosts] = useState({user: []})
@@ -16,22 +18,27 @@ const Table = () => {
 
     return (
         <div>
-            <ReactBootStrap.Table striped bordered hover>
+            <ReactBootStrap.Table striped bordered hover className="table">
                 <thead>
                 <tr>
-                    <th></th>
-                    <th>ID</th>
-                    <th>Title</th>
-                    <th>Body</th>
+                    <th class="table-light" width="140px">아이디 번호</th>
+                    <th class="table-primary">이메일</th>
+                    <th class="table-light">내용</th>
+                    <th class="table-warning" width="250px">수정 / 삭제</th>
                 </tr>
                 </thead>
                 <tbody>
                 {
                     posts.user && posts.user.map((item) => (
                         <tr key={item.id}>
-                            <td>{item.id}</td>
-                            <td>{item.title}</td>
-                            <td>{item.body}</td>
+                            <td class="table-light" valign="middle">{item.id}</td>
+                            <td class="table-primary" valign="middle">{item.title}</td>
+                            <td class="table-light" valign="middle">{item.body}</td>
+                            <td class="table-warning" valign="middle">
+                                <Button variant="info" className="mar">Info</Button>
+                                <Button variant="warning" className="mar">Update</Button>
+                                <Button variant="danger">Delete</Button>
+                            </td>
                         </tr>
                     ))
                 }
