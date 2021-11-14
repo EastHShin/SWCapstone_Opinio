ae93fd137a66f117ce61e66a23168ff2bc390924
EastHShin <xldk78@gmail.com>
2021-11-14 15:02:55 +0900

    watering controller 및 식물 프로필 조회 테스트코드 추가, 식물 프로필 조회 컨트롤러 추가

---
+++ back-end/plantrowth/src/main/java/com/opinio/plantrowth/api/dto/plant/PlantManageDto.java
@@ -0,0 +1,37 @@
+package com.opinio.plantrowth.api.dto.plant;
+
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.opinio.plantrowth.domain.Plant;
+import lombok.Getter;
+
+import javax.persistence.Column;
+import java.time.LocalDate;
+
+@Getter
+public class PlantManageDto {
+    private Long plant_id;
+    private String plant_species;
+    private String plant_name;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "Asia/Seoul")
+    private LocalDate plant_birth;
+
+    private Integer plant_exp;
+    private String file_name;
+    private Integer water_supply;
+    private Integer alarm_cycle;
+    private Integer remain_cycle;
+
+    public PlantManageDto(Plant plant) {
+        plant_id = plant.getId();
+        plant_species = plant.getPlantSpecies();
+        plant_name = plant.getPlantName();
+        plant_birth = plant.getPlantBirth();
+        plant_exp = plant.getPlantExp();
+        file_name = plant.getFileName();
+        water_supply = plant.getWaterSupply();
+        alarm_cycle = plant.getAlarmCycle();
+        remain_cycle = plant.getRemainCycle();
+    }
+
+}
