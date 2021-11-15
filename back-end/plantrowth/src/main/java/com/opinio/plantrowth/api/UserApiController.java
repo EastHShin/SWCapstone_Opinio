0e467dcb99626ccafc8f995b61e89f6d651f88e2
msY <jleeap@ajou.ac.kr>
2021-11-15 19:39:12 +0900

    userAPI controller userId 반환도록 수정

--- back-end/plantrowth/src/main/java/com/opinio/plantrowth/api/UserApiController.java
+++ back-end/plantrowth/src/main/java/com/opinio/plantrowth/api/UserApiController.java
@@ -22,33 +22,34 @@ import java.util.Map;
 @RestController
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 public class UserApiController {
-    private final UserService userService;
     private final JwtTokenProvider jwtTokenProvider;
+    private final UserService userService;
 
-
-
-    @PostMapping("/api/auth/join") //회원가입
-    public ResponseEntity join(@RequestBody JoinDTO user){
+    @PostMapping("/api/auth/join")
+    public ResponseEntity join(@RequestBody JoinDTO user) {
         Long result = userService.join(user);
 
-        return result != null?
-                ResponseEntity.ok().body("회원가입 성공"):
+        return result != null ?
+                ResponseEntity.ok().body("회원가입 성공") :
                 ResponseEntity.badRequest().build();
 
     }
 
-   @PostMapping("/api/auth/login") //로그인
-    public ResponseEntity<?> login(HttpServletResponse response, @RequestBody LoginDTO user){
+    @PostMapping("/api/auth/login") //로그인
+    public ResponseEntity<?> login(HttpServletResponse response, @RequestBody LoginDTO user) {
         User member = userService.login(user);
         Message message = new Message();
         HttpHeaders headers = new HttpHeaders();
         String token = jwtTokenProvider.createToken(member.getName(), member.getRoles());
         response.setHeader("Authorization", token);
-        return  ResponseEntity.ok().body("로그인 성공");
+        message.setStatus(Message.StatusEnum.OK);
+        message.setMessage("카카오 로그인 성공");
+        message.setData(member.getId());
+        return new ResponseEntity<>(message, headers, HttpStatus.OK);
     }
 
     @PostMapping("/api/auth/kakao") //소셜 로그인
-    public ResponseEntity<Message> kakaoLogin(@RequestBody KakaoDTO user){
+    public ResponseEntity<?> kakaoLogin(@RequestBody KakaoDTO user) {
         Long result = userService.kakaoLogin(user);
         Message message = new Message();
         HttpHeaders headers = new HttpHeaders();
@@ -58,60 +59,60 @@ public class UserApiController {
         message.setData(result);
         return new ResponseEntity<>(message, headers, HttpStatus.OK);
     }
+
     @GetMapping("/api/user/{user-id}")
-    public ResponseEntity<?> lookUpUser(@PathVariable("user-id") Long id){
-       User member = userService.findUser(id);
-       UserLookUpDTO user = new UserLookUpDTO();
-       user.setUser_name(member.getName());
-       user.setUser_birth(member.getBirth());
-       user.setEmail(member.getEmail());
-       user.setPoint(member.getPoint());
-       user.setPlantNum(member.getPlantNum());
-       user.setMaxPlantNum(member.getMaxPlantNum());
-       Message message = new Message();
-       HttpHeaders headers = new HttpHeaders();
-       headers.setContentType(new MediaType("application", "json", Charset.forName("UTF-8")));
-       message.setStatus(Message.StatusEnum.OK);
-       message.setMessage("회원 조회 성공");
-       message.setData(user);
+    public ResponseEntity<?> lookUpUser(@PathVariable("user-id") Long id) {
+        User member = userService.findUser(id);
+        UserLookUpDTO user = new UserLookUpDTO();
+        user.setUser_name(member.getName());
+        user.setUser_birth(member.getBirth());
+        user.setEmail(member.getEmail());
+        user.setPoint(member.getPoint());
+        user.setPlantNum(member.getPlantNum());
+        user.setMaxPlantNum(member.getMaxPlantNum());
+        Message message = new Message();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(new MediaType("application", "json", Charset.forName("UTF-8")));
+        message.setStatus(Message.StatusEnum.OK);
+        message.setMessage("회원 조회 성공");
+        message.setData(user);
 
-       return new ResponseEntity<>(message, headers, HttpStatus.OK);
+        return new ResponseEntity<>(message, headers, HttpStatus.OK);
     }
-    @PutMapping("/api/user/{user-id}")
-    public ResponseEntity<?> updateUser(@PathVariable("user-id") Long id, @RequestBody UserUpdateDTO user){
-       userService.updateUser(id, user);
-       Message message = new Message();
-       HttpHeaders headers = new HttpHeaders();
-       headers.setContentType(new MediaType("application", "json", Charset.forName("UTF-8")));
-       message.setStatus(Message.StatusEnum.OK);
-       message.setMessage("회원 정보가 수정되었습니다.");
-       message.setData(user);
 
-       return new ResponseEntity<>(message, headers, HttpStatus.OK);
+    @PutMapping("/api/user/{user-id}")
+    public ResponseEntity<?> updateUser(@PathVariable("user-id") Long id, @RequestBody UserUpdateDTO user) {
+        userService.updateUser(id, user);
+        Message message = new Message();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(new MediaType("application", "json", Charset.forName("UTF-8")));
+        message.setStatus(Message.StatusEnum.OK);
+        message.setMessage("회원 정보가 수정되었습니다.");
+        message.setData(user);
+
+        return new ResponseEntity<>(message, headers, HttpStatus.OK);
     }
 
     @DeleteMapping("api/user/{user-id}")
-    public ResponseEntity<?> deleteUser(@PathVariable("user-id") Long id){
+    public ResponseEntity<?> deleteUser(@PathVariable("user-id") Long id) {
         Long result = userService.deleteUser(id);
-        return result !=null?
-                ResponseEntity.ok().body("회원 탈퇴 성공"):
+        return result != null ?
+                ResponseEntity.ok().body("회원 탈퇴 성공") :
                 ResponseEntity.badRequest().build();
     }
+
     @PostMapping("/user/test")
-    public Map userResponseTest(){
-       Map<String, String> result = new HashMap<>();
-       result.put("result", "user ok");
-       return result;
+    public Map userResponseTest() {
+        Map<String, String> result = new HashMap<>();
+        result.put("result", "user ok");
+        return result;
     }
 
-
-
     @PostMapping("/admin/test")
-    public Map adminResponseTest(){
-       Map<String, String> result = new HashMap<>();
-       result.put("result", "admin ok");
-       return result;
+    public Map adminResponseTest() {
+        Map<String, String> result = new HashMap<>();
+        result.put("result", "admin ok");
+        return result;
     }
 
 }
-
