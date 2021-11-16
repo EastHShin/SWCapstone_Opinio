410d4860a58996552ad92b2686719c75f24be778
EastHShin <xldk78@gmail.com>
2021-11-16 17:37:09 +0900

    식물일기 등록 dto 수정

--- back-end/plantrowth/src/main/java/com/opinio/plantrowth/api/dto/diary/CreateDiaryDTO.java
+++ back-end/plantrowth/src/main/java/com/opinio/plantrowth/api/dto/diary/CreateDiaryDTO.java
@@ -3,11 +3,13 @@ package com.opinio.plantrowth.api.dto.diary;
 import com.opinio.plantrowth.domain.PlantDiary;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 
 @Getter
+@Setter
 @NoArgsConstructor
 public class CreateDiaryDTO {
     private String title;
