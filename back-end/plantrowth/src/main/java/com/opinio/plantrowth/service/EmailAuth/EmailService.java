d829031a30eadc919ad3787e109ab48f0873c009
EastHShin <xldk78@gmail.com>
2021-11-18 15:33:15 +0900

    이메일 인증 토큰 저장소 구현, 인증 로직 구현

--- back-end/plantrowth/src/main/java/com/opinio/plantrowth/service/EmailAuth/EmailService.java
+++ back-end/plantrowth/src/main/java/com/opinio/plantrowth/service/EmailAuth/EmailService.java
@@ -1,31 +1,39 @@
 package com.opinio.plantrowth.service.EmailAuth;
 
+import com.opinio.plantrowth.domain.ConfirmationToken;
+import com.opinio.plantrowth.repository.ConfirmationTokenRepository;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.mail.MailException;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.mail.Message;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
+import java.time.LocalDateTime;
+import java.util.Optional;
 import java.util.Random;
 
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 @Service
 public class EmailService {
 
     private final JavaMailSender emailSender;
+    private final ConfirmationTokenRepository confirmationTokenRepository;
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
-    public static final String ePw = createKey();
+//    public static final String ePw = createKey();
 
     private MimeMessage createMessage(String to) throws Exception {
+        String ePw = createKey();
+        saveToken(to, ePw);
+
         logger.info("보내는 대상 : " + to);
         logger.info("인증 번호 : " + ePw);
         MimeMessage message = emailSender.createMimeMessage();
-
         String code = createCode(ePw);
         message.addRecipients(Message.RecipientType.TO, to);
         message.setSubject("Plantrowth 확인 코드: " + code);
@@ -39,6 +47,12 @@ public class EmailService {
         return message;
     }
 
+    @Transactional
+    public void saveToken(String to, String ePw) {
+        ConfirmationToken confirmationToken = ConfirmationToken.createEmailConfirmationToken(to, ePw);
+        confirmationTokenRepository.save(confirmationToken);
+    }
+
     public void sendSimpleMessage(String to) throws Exception {
         MimeMessage message = createMessage(to);
         try {
@@ -48,6 +62,16 @@ public class EmailService {
             throw new IllegalArgumentException("이메일 인증 오류");
         }
     }
+
+    @Transactional
+    public ConfirmationToken findValidToken(String code) {
+        ConfirmationToken confirmationToken = confirmationTokenRepository.findByCodeAndExpirationDateAfterAndExpired(code, LocalDateTime.now(), false)
+                .orElseThrow(() -> new IllegalArgumentException("Token을 찾을 수 없습니다."));
+        confirmationToken.useToken();
+        return confirmationToken;
+    }
+
+
 
     private String createCode(String ePw) {
         return ePw.substring(0,3) + "-" + ePw.substring(3, 6);
