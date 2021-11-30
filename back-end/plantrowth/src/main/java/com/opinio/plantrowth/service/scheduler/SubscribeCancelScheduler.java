080d12f50620496ae7ed39359349280e0785c397
EastHShin <xldk78@gmail.com>
2021-11-30 15:26:26 +0900

    feat: 매일 자정마다 질병진단 구독 기간이 끝난 사용자의 구독여부 필드를 false로 만드는 기능 구현

--- back-end/plantrowth/src/main/java/com/opinio/plantrowth/service/scheduler/SubscribeCancelScheduler.java
+++ back-end/plantrowth/src/main/java/com/opinio/plantrowth/service/scheduler/SubscribeCancelScheduler.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.opinio.plantrowth.domain.payment.Subscription;
 import com.opinio.plantrowth.domain.user.User;
@@ -13,21 +14,22 @@ import com.opinio.plantrowth.repository.payment.SubscriptionRepository;
 import com.opinio.plantrowth.repository.user.UserRepository;
 
 import lombok.RequiredArgsConstructor;
-//
-// @RequiredArgsConstructor
-// @Component
-// public class SubscribeCancelScheduler {
-//
-// 	// private final UserRepository userRepository;
-// 	// private final SubscriptionRepository subscriptionRepository;
-// 	//
-// 	// @Scheduled(cron = "0 0 0 * * *")
-// 	// public void subscriptionSchedule() {
-// 	// 	List<Subscription> subscriptionList = subscriptionRepository.findAll();
-// 	// 	List<Subscription> expiredList = subscriptionList.stream()
-// 	// 		.filter(subscription -> subscription.getEnd().isAfter(LocalDate.now()))
-// 	// 		.collect(Collectors.toList());
-// 	// }
-//
-//
-// }
+
+@RequiredArgsConstructor
+@Component
+public class SubscribeCancelScheduler {
+
+	private final SubscriptionRepository subscriptionRepository;
+
+	@Transactional
+	@Scheduled(cron = "0 0 16 * * *")
+	public void subscriptionSchedule() {
+		List<Subscription> subscriptionList = subscriptionRepository.findAll();
+		subscriptionList.stream()
+			.filter(subscription -> subscription.getEnd().isAfter(LocalDate.now()))
+			.forEach(expiredSubscription -> expiredSubscription.getUser().setSubscription(false));
+
+	}
+
+
+}
