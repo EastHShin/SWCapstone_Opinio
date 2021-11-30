9b9a8a84e6188af0068a91f5a1f03f7c8171a11e
EastHShin <xldk78@gmail.com>
2021-11-30 16:29:17 +0900

    fix: DTO수정

--- back-end/plantrowth/src/main/java/com/opinio/plantrowth/api/dto/community/comment/CommentCUDto.java
+++ back-end/plantrowth/src/main/java/com/opinio/plantrowth/api/dto/community/comment/CommentCUDto.java
@@ -1,10 +1,16 @@
 package com.opinio.plantrowth.api.dto.community.comment;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDate;
 
 @Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class CommentCUDto {
     private String content;
     private LocalDate date;
