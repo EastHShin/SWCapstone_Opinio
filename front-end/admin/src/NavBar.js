7c34c17c521fb2935d6525602e2c47815d150114
skysaver00 <skysaver00@naver.com>
2021-11-30 15:28:09 +0900

    광고 프론트엔드 비활성화

--- front-end/admin/src/NavBar.js
+++ front-end/admin/src/NavBar.js
@@ -8,7 +8,7 @@ function NavBar() {
             <ul>
                 <li className="inline"><Link to="/administrator" className="inline">로그아웃</Link></li>
                 <li className="inline"><Link to="/administrator/front" className="inline">메인화면</Link></li>
-                <li className="inline"><Link to="/administrator/ad" className="inline">광고관리</Link></li>
+                {/*<li className="inline"><Link to="/administrator/ad" className="inline">광고관리</Link></li>*/}
                 <li className="inline"><Link to="/administrator/user" className="inline">유저관리</Link></li>
                 <li className="inline"><Link to="/administrator/report" className="inline">신고관리</Link></li>
                 <li className="inline"><Link to="/administrator/board" className="inline">게시글/댓글</Link></li>
