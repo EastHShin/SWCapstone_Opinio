e602ad9c012059a28adb9136d84f9bbcff4db7d8
EastHShin <xldk78@gmail.com>
2021-12-03 15:18:07 +0900

    fix(mainPage): 메인페이지의 식물 프로필을 레벨순으로 정렬해서 보내주게 수정

--- back-end/plantrowth/src/main/java/com/opinio/plantrowth/api/controller/MainPageApiController.java
+++ back-end/plantrowth/src/main/java/com/opinio/plantrowth/api/controller/MainPageApiController.java
@@ -4,15 +4,18 @@ import com.opinio.plantrowth.domain.plant.Plant;
 import com.opinio.plantrowth.domain.user.User;
 import com.opinio.plantrowth.service.plant.PlantService;
 import com.opinio.plantrowth.service.user.UserService;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -20,71 +23,73 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class MainPageApiController {
 
-    private final UserService userService;
-    private final PlantService plantService;
+	private final UserService userService;
+	private final PlantService plantService;
 
-//    @PostConstruct
-//    public void testInit(){
-//        User user = new User();
-//        user.setEmail("fff");
-//        user.setPassword("eerre");
-//        user.setName("east");
-//        User savedUser = userRepository.save(user);
-//    }
+	//    @PostConstruct
+	//    public void testInit(){
+	//        User user = new User();
+	//        user.setEmail("fff");
+	//        user.setPassword("eerre");
+	//        user.setName("east");
+	//        User savedUser = userRepository.save(user);
+	//    }
 
-    @GetMapping("/api/main/{user-id}")
-    public ResponseEntity<MainPageResult> getMainPage(@PathVariable("user-id") Long id) {
-        User user = userService.findUser(id);
-        List<Plant> plants = plantService.findPlants(id);
-        //List<Board> boards = boardService.findBestPost();      나중에 게시판 구현하면 수정
-        return new ResponseEntity<>(new MainPageResult<MainPageDto>(new MainPageDto(user,plants)), HttpStatus.OK);
-    }
+	@GetMapping("/api/main/{user-id}")
+	public ResponseEntity<MainPageResult> getMainPage(@PathVariable("user-id") Long id) {
+		User user = userService.findUser(id);
+		List<Plant> plants = plantService.findPlants(id);
+		//List<Board> boards = boardService.findBestPost();      나중에 게시판 구현하면 수정
 
-    @Getter
-    @AllArgsConstructor
-    static class MainPageResult<T>{
-        private T data;
-    }
+		return new ResponseEntity<>(new MainPageResult<MainPageDto>(new MainPageDto(user, plants)), HttpStatus.OK);
+	}
 
-    @Getter
-    public static class MainPageDto{
-        private Long user_id;
-        private String user_name;
-        private Integer point;
-        private Integer max_plant_num;
-        private Integer plant_num;
-        private List<MainPagePlantDto> plants;
-        //private List<Board> boards;
+	@Getter
+	@AllArgsConstructor
+	static class MainPageResult<T> {
+		private T data;
+	}
 
-        public MainPageDto(User user, List<Plant> plantList) { //List<Board> boardList
-            user_id = user.getId();
-            user_name = user.getName();
-            point = user.getPoint();
-            plant_num = user.getPlantNum();
-            max_plant_num = user.getMaxPlantNum();
+	@Getter
+	public static class MainPageDto {
+		private Long user_id;
+		private String user_name;
+		private Integer point;
+		private Integer max_plant_num;
+		private Integer plant_num;
+		private List<MainPagePlantDto> plants;
+		//private List<Board> boards;
 
-            plants = plantList.stream()
-                    .map(plant -> new MainPagePlantDto(plant))
-                    .collect(Collectors.toList());
-        }
-    }
+		public MainPageDto(User user, List<Plant> plantList) { //List<Board> boardList
+			user_id = user.getId();
+			user_name = user.getName();
+			point = user.getPoint();
+			plant_num = user.getPlantNum();
+			max_plant_num = user.getMaxPlantNum();
 
-    @Getter
-    static class MainPagePlantDto{
-        private Long plant_id;
-        private String plant_name;
-        private Integer plant_exp;
-        private Integer plant_level;
-        private String file_name;
+			plants = plantList.stream()
+				.map(plant -> new MainPagePlantDto(plant))
+				.sorted(Comparator.comparing(MainPagePlantDto::getPlant_level).reversed())
+				.collect(Collectors.toList());
+		}
+	}
 
-        public MainPagePlantDto(Plant plant) {
-            plant_id = plant.getId();
-            plant_name = plant.getPlantName();
-            plant_exp = plant.getPlantExp();
-            plant_level = plant.getPlantLevel();
-            file_name = plant.getFileName();
-        }
-    }
+	@Getter
+	static class MainPagePlantDto {
+		private Long plant_id;
+		private String plant_name;
+		private Integer plant_exp;
+		private Integer plant_level;
+		private String file_name;
+
+		public MainPagePlantDto(Plant plant) {
+			plant_id = plant.getId();
+			plant_name = plant.getPlantName();
+			plant_exp = plant.getPlantExp();
+			plant_level = plant.getPlantLevel();
+			file_name = plant.getFileName();
+		}
+	}
     /* 게시판 구현하면 풀기
     @Getter
     static class MainPageBoardDto{
