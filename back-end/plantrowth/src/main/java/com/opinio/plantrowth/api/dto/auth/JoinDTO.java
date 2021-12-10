cc47c8a71433f60426cb0a7f72b205c121fc9078
minsang <jleeap@ajou.ac.kr>
2021-12-10 13:34:13 +0900

    testcode & dto update

--- back-end/plantrowth/src/main/java/com/opinio/plantrowth/api/dto/auth/JoinDTO.java
+++ back-end/plantrowth/src/main/java/com/opinio/plantrowth/api/dto/auth/JoinDTO.java
@@ -1,11 +1,16 @@
 package com.opinio.plantrowth.api.dto.auth;
 
+import com.opinio.plantrowth.domain.user.User;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDate;
 
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor
 @Setter
 public class JoinDTO {
     private String user_name;
@@ -13,4 +18,10 @@ public class JoinDTO {
     private String email;
     private String password;
     private String fcm_access_token;
+    public JoinDTO(User user){
+        user_name = user.getUsername();
+        user_birth = user.getBirth();
+        email = user.getEmail();
+        password = user.getPassword();
+    }
 }
