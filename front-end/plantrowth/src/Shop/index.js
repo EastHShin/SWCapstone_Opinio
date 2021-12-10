3d95bfd465515f31605571fed3d6f88fd50fd9ae
sudalasan <sudalasan@gmail.com>
2021-12-10 18:16:46 +0900

    결제 약관 추가

--- front-end/plantrowth/src/Shop/index.js
+++ front-end/plantrowth/src/Shop/index.js
@@ -22,6 +22,7 @@ import {
 import { useSelector, useDispatch } from 'react-redux';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useNavigation, useIsFocused } from '@react-navigation/native';
+import { ScrollView } from 'react-native-gesture-handler';
 
 const screenHeight = Dimensions.get('window').height;
 const screenWidth = Dimensions.get('window').width;
@@ -491,19 +492,19 @@ ShopScreen = () => {
         <View
           style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}
         >
-          <View style={styles.buyResultModalWrapper}>
-            <Text style={[styles.buyResultText, { fontSize: 18 }]}>
+          <View style={[styles.buyResultModalWrapper, {height: termsType == 'SLOT' ? screenHeight*0.3 : screenHeight*0.7} ]}>
+            <Text style={[styles.buyResultText, { fontSize: 18, marginBottom: 10}]}>
               결제 약관
             </Text>
             {termsType == 'SLOT'
               ? (
-                <View>
-                  <Text>슬롯</Text>
+                <View >
+                  <Text style={{ color: '#363636', margin: 5, textAlign: 'justify', fontSize: 13 }}>슬롯은 구매 후 환불이 불가한 상품입니다</Text>
                 </View>
               ) : (
-                <View>
+                <ScrollView>
                   <Text style={{ color: '#363636', fontFamily: 'NanumGothicBold', fontSize: 16, margin: 5 }}>
-                    구독 결제 시점과 이용 기간
+                    {`\n구독 결제 시점과 이용 기간`}
                   </Text>
                   <Text style={{ color: '#363636', margin: 5, textAlign: 'justify', fontSize: 13 }}>
                     구독 결제는 한 달 단위로 결제되고, 해당 날짜가 되는 자정에 구독이 해지됩니다.
@@ -512,17 +513,21 @@ ShopScreen = () => {
                   <Text style={{ color: '#363636', fontFamily: 'NanumGothicBold', fontSize: 16, margin: 5 }}>
                     {'\n'}환불 정책
                   </Text>
-                  {/* <Text style={{ color: '#363636', margin: 5, textAlign: 'justify', fontSize: 13}}>
-                  질병진단 내역에서 구독일 이후로 질병진단 실행 시:{`\n  `}
-                  <Text style={{ color: '#ef5350', margin: 5, textAlign: 'justify', fontSize: 13}}>환불 불가{`\n`}</Text>
-                  결제일 이후 3일이 지나기 전까지:{`\n  `}
-                  결제 금액의 100% 환불{`\n`}
-                  결제일 이후 3일이 지난 시점에서 14일이 지나기 전까지:{`\n  `}
-                  결제 금액의 50% 환불{`\n`}
-                  결제일 이후 14일이 지난 시점부터:{`\n  `}
-                  환불 불가{`\n`}
-                  </Text> */}
-                </View>
+                  <Text style={{ color: '#363636', textAlign: 'justify', fontSize: 12 }}>
+                    질병진단 내역에서 구독일 이후로 질병진단 실행 시:
+                  </Text>
+                  <Text style={{ color: '#ef5350', textAlign: 'justify', fontSize: 13 }}>{`  환불 불가\n`}</Text>
+                  <Text style={{ color: '#363636', textAlign: 'justify', fontSize: 12 }}>
+                    결제일 이후 3일이 지나기 전까지:{`\n  `}
+                    결제 금액의 100% 환불{`\n\n`}
+                    결제일 이후 3일이 지난 시점에서 14일이 지나기 전까지:{`\n  `}
+                    결제 금액의 50% 환불{`\n\n`}
+                    결제일 이후 14일이 지난 시점부터:
+                  </Text>
+                  <Text style={{ color: '#ef5350', textAlign: 'justify', fontSize: 13 }}>
+                    {`  환불 불가\n`}
+                  </Text>
+                </ScrollView>
               )
             }
             <TouchableOpacity
