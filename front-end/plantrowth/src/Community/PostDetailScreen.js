9488e68c42a1b4584310af83f6a87c76b1e8c189
bin0318 <bin55@ajou.ac.kr>
2021-12-13 22:09:16 +0900

    닉네임 중복체크 수정

--- front-end/plantrowth/src/Community/PostDetailScreen.js
+++ front-end/plantrowth/src/Community/PostDetailScreen.js
@@ -248,6 +248,7 @@ const PostDetailScreen = ({ route, navigation }) => {
   const renderComment = comment => {
     if (comment) {
       return comment.map((item, index) => {
+        if(!item.is_blocked){
         return (
           <View
             key={index}
@@ -303,8 +304,10 @@ const PostDetailScreen = ({ route, navigation }) => {
             </TouchableOpacity>
           </View>
         );
+      }
       });
     }
+  
   };
   //댓글
 
