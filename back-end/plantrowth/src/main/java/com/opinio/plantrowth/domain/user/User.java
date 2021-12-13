d58b1c0829214e98ebfef04e2f470159a2a485ac
EastHShin <xldk78@gmail.com>
2021-12-14 01:25:26 +0900

    fix: 유저 도메인 신고와 양방향 매핑

--- back-end/plantrowth/src/main/java/com/opinio/plantrowth/domain/user/User.java
+++ back-end/plantrowth/src/main/java/com/opinio/plantrowth/domain/user/User.java
@@ -17,6 +17,7 @@ import java.util.stream.Collectors;
 import com.opinio.plantrowth.domain.community.Board;
 import com.opinio.plantrowth.domain.community.BoardLike;
 import com.opinio.plantrowth.domain.community.Comment;
+import com.opinio.plantrowth.domain.community.Report;
 import com.opinio.plantrowth.domain.payment.PaymentRecord;
 import com.opinio.plantrowth.domain.payment.PointRecord;
 import com.opinio.plantrowth.domain.payment.Subscription;
@@ -82,6 +83,10 @@ public class User implements UserDetails {
 	@OneToMany(fetch = FetchType.LAZY, cascade = {CascadeType.MERGE, CascadeType.REMOVE}, mappedBy = "user")
 	@Builder.Default
 	List<Subscription> subscriptions = new ArrayList<>();
+
+	@OneToMany(fetch = FetchType.LAZY, cascade = {CascadeType.MERGE, CascadeType.REMOVE}, mappedBy = "user")
+	@Builder.Default
+	List<Report> reports = new ArrayList<>();
 
 	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
